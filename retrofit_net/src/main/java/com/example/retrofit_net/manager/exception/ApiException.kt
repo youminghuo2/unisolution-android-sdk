e7271bd4e536423c8f9febbb5da38e3cae1153d1
zhuchenyi <zcy@unisolution.cn>
2024-01-25 14:37:04 +0800

    新增空null subcode支持

--- retrofit_net/src/main/java/com/example/retrofit_net/manager/exception/ApiException.kt
+++ retrofit_net/src/main/java/com/example/retrofit_net/manager/exception/ApiException.kt
@@ -4,10 +4,10 @@ import java.io.IOException
 
 open class ApiException : Exception {
     var errCode: String
-    var errSubCode: String=""
+    var errSubCode: String?=""
     var errMsg: String
 
-    constructor(code: String, subCode: String, msg: String, e: Throwable? = null) : super(e) {
+    constructor(code: String, subCode: String?, msg: String, e: Throwable? = null) : super(e) {
         this.errCode = code
         this.errSubCode = subCode
         this.errMsg = msg
