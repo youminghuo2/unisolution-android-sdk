764c14435fd251e727549111fe605166936878b9
朱晨艺 <zcy@unisolution.cn>
2024-08-21 10:51:08 +0800

    部分代码

--- app/src/main/java/com/example/xuece_android_network/activity/MainActivity.kt
+++ app/src/main/java/com/example/xuece_android_network/activity/MainActivity.kt
@@ -2,51 +2,56 @@ package com.example.xuece_android_network.activity
 
 import android.Manifest
 import android.app.Dialog
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Handler
 import android.os.Looper
+import android.provider.MediaStore
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
+import androidx.core.content.FileProvider
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.lifecycle.lifecycleScope
 import com.dylanc.longan.Logger
-import com.dylanc.longan.activity
 import com.dylanc.longan.context
 import com.dylanc.longan.launchAppSettings
 import com.dylanc.longan.lifecycleOwner
 import com.dylanc.longan.logDebug
 import com.example.module_frame.dialog.DialogManager
-import com.example.module_frame.dialog.PermissionExplainHelper
 import com.example.module_frame.dialog.PermissionExplainHelper.dismissExplain
 import com.example.module_frame.dialog.PermissionExplainHelper.showExplain
-import com.example.module_frame.dialog.XueCeDialogBuilder
 import com.example.module_frame.dialog.builder.FlutterDialogFragment
 import com.example.module_frame.dialog.builder.SingleDialogBuilder
-import com.example.module_frame.dialog.builder.XueCeDialogFragment
 import com.example.module_frame.entity.PermissionEntity
 import com.example.module_frame.entity.PermissionListEntity
-import com.example.module_frame.viewBinding.BaseViewBindingActivity
-import com.example.xuece_android_network.common.ComDaraStore
 import com.example.module_frame.utils.CommonUtils
 import com.example.module_frame.utils.CommonUtils.processPermissions
 import com.example.module_frame.utils.dataStore
+import com.example.module_frame.viewBinding.BaseViewBindingActivity
+import com.example.xuece_android_network.common.ComDaraStore
 import com.example.xuece_android_network.common.TagData
 import com.example.xuece_android_network.databinding.ActivityMainBinding
+import com.example.xuece_android_network.utils.PhotoBitmapUtils
 import com.example.xuece_android_network.viewModel.UserCenterViewModel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
-import java.io.File.separator
+import java.io.File
+
 
 class MainActivity : BaseViewBindingActivity<ActivityMainBinding>() {
 
     private val viewModel by viewModels<UserCenterViewModel>()
     private lateinit var dialogBuilder: SingleDialogBuilder
 
-    private var dialogMsg=""
+    private var dialogMsg = ""
+
+
+    private var fileName = ""
 
     override fun initView() {
 
@@ -69,7 +74,7 @@ class MainActivity : BaseViewBindingActivity<ActivityMainBinding>() {
             val handler = Handler(Looper.getMainLooper())
             val runnable = object : Runnable {
                 override fun run() {
-                    val dialogBuilder = DialogManager.getDialog(this@MainActivity){
+                    val dialogBuilder = DialogManager.getDialog(this@MainActivity) {
 
                     }
                     if (dialogBuilder.isDialogShowing() != true) {
@@ -105,8 +110,8 @@ class MainActivity : BaseViewBindingActivity<ActivityMainBinding>() {
                 }
 
             lifecycleScope.launch {
-                usertypeCodeFlow.collect{token->
-                    binding.dataStoreTv.text=token
+                usertypeCodeFlow.collect { token ->
+                    binding.dataStoreTv.text = token
                 }
             }
         }
@@ -119,20 +124,35 @@ class MainActivity : BaseViewBindingActivity<ActivityMainBinding>() {
         }
 
         /**
-         * 申请权限
+         * 申请权限 多个权限,RequestMultiplePermissions
          */
         binding.permissionBtn.setOnClickListener {
-            /**
-             * 测试权限说明，多权限申请 案例
-             */
-            val permissionList= listOf(
-                PermissionListEntity(listOf(Manifest.permission.BLUETOOTH_SCAN,Manifest.permission.BLUETOOTH_ADVERTISE,Manifest.permission.BLUETOOTH_CONNECT) ,PermissionEntity("蓝牙权限","当您在我们的产品中使蓝牙")),
-                PermissionListEntity(listOf(Manifest.permission.CAMERA) , PermissionEntity("相机权限","当您在我们的产品中使用数据上传功能时")),
-                PermissionListEntity(listOf(Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION) , PermissionEntity("地理位置权限","当您在我们的产品中使用地理位置权限"))
+
+//             测试权限说明，多权限申请 案例
+            val permissionList = listOf(
+                PermissionListEntity(
+                    listOf(
+                        Manifest.permission.BLUETOOTH_SCAN,
+                        Manifest.permission.BLUETOOTH_ADVERTISE,
+                        Manifest.permission.BLUETOOTH_CONNECT
+                    ), PermissionEntity("蓝牙权限", "当您在我们的产品中使蓝牙")
+                ),
+                PermissionListEntity(
+                    listOf(Manifest.permission.CAMERA),
+                    PermissionEntity("相机权限", "当您在我们的产品中使用数据上传功能时")
+                ),
+                PermissionListEntity(
+                    listOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ), PermissionEntity("地理位置权限", "当您在我们的产品中使用地理位置权限")
+                )
             )
 
-            val multipleList=CommonUtils.checkSelfPermissionMultiple(context,
-                permissionList)
+            val multipleList = CommonUtils.checkSelfPermissionMultiple(
+                context,
+                permissionList
+            )
 
             if (multipleList.isNotEmpty()) {
                 val (explainList, requireList) = processPermissions(multipleList)
@@ -148,35 +168,128 @@ class MainActivity : BaseViewBindingActivity<ActivityMainBinding>() {
             }
         }
 
+        /**
+         * 申请单个权限 ,RequestPermission
+         */
+        binding.permissionBtn2.setOnClickListener {
+            val permission = Manifest.permission.CAMERA
+            when (CommonUtils.checkSelfPermission(context, permission)) {
+                true -> {
+                    //授权
+                    Toast.makeText(context, "已经授予权限", Toast.LENGTH_SHORT).show()
+                }
+
+                else -> {
+                    //未授权，开始授权
+                    mPermissionLauncher.launch(permission)
+                }
+            }
+
+        }
+
+        /**
+         *跳转系统拍照，假设已经授予了camera
+         */
+        binding.cameraBtn.setOnClickListener {
+            Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent->
+                try {
+                    takePictureIntent.resolveActivity(packageManager)?.also {
+                         val photoURI = createTempPictureUri()
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                        takePhotoLauncher.launch(photoURI)
+                    }
+
+                }catch (e:Exception){
+                    Logger(TagData.MainActivity).logDebug("error ---> ${e.message}")
+                }
+            }
+        }
+
+
 
     }
-
 
 
     /**
      * 回调处监听，ActivityResultAPI
      */
     private val multiPermissionLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
-                it ->
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { it ->
             dismissExplain()
-            if (it.values.any { !it }){
+            if (it.values.any { !it }) {
+                //常规弹窗
 //                XueCeDialogBuilder(this).setTitle("权限申请").setMessage(dialogMsg).setNegativeButton("取消").setCancelable(false).setPositiveButton("确定",object:XueCeDialogFragment.OnClickListener{
 //                    override fun onClick(dialog: Dialog?) {
 //                        dialog?.dismiss()
 //                        launchAppSettings()
 //                    }
 //                }).show()
-               FlutterDialogFragment("权限申请",dialogMsg,"取消","确定",false,object :FlutterDialogFragment.OnClickListener{
-                   override fun onClick(dialog: Dialog?) {
-                       dialog?.dismiss()
-                       launchAppSettings()
-                   }
-               }).show(supportFragmentManager,"")
+                //可以在Flutter弹窗
+                FlutterDialogFragment(
+                    "权限申请",
+                    dialogMsg,
+                    "取消",
+                    "确定",
+                    false,
+                    object : FlutterDialogFragment.OnClickListener {
+                        override fun onClick(dialog: Dialog?) {
+                            dialog?.dismiss()
+                            launchAppSettings()
+                        }
+                    }).show(supportFragmentManager, "")
 
             }
         }
 
+    //监听单个权限
+    private val mPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                //已授权
+                Toast.makeText(context, "已成功授予权限", Toast.LENGTH_SHORT).show()
+            } else {
+                //未授权
+                Toast.makeText(context, "授予权限失败", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+   //跳转拍照
+   private val takePhotoLauncher=registerForActivityResult(ActivityResultContracts.TakePicture()) {isSaved ->
+       if (isSaved ){
+           //保存成功
+           val filepath: String = PhotoBitmapUtils.amendRotatePhoto(fileName, this)
 
 
+       }
+
+   }
+//           result ->
+//       if (result.resultCode==Activity.RESULT_OK){
+//       //要在这里面进行
+//       result.data!!.data?.let{ uri->
+//           //选择显示的图片
+//           val bitmap=getBitMapFromUri(uri)
+//           binding.pictureImg.setImageBitmap(bitmap)
+//       }
+//    =registerForActivityResult(ActivityResultContracts.TakePicture()){isBoolean->{
+//
+//   }
+
+//    private val takePhotoLauncher = registerForActivityResult<Uri, Boolean>(
+//           TakePicture(),
+//           object : ActivityResultCallback<Boolean?> {
+//               fun onActivityResult(result: Boolean) {
+//
+//                   // do what you need with the uri here ...
+//               }
+//           })
+fun createTempPictureUri(fileName: String = "picture_${System.currentTimeMillis()}", fileExtension: String = ".png"): Uri {
+    val tempFile = File.createTempFile(fileName, fileExtension, cacheDir).apply { createNewFile()}
+    this.fileName=fileName
+
+    return FileProvider.getUriForFile(this, "com.example.xuece_android_network.fileprovider", tempFile)
 }
+
+   }
+
+
