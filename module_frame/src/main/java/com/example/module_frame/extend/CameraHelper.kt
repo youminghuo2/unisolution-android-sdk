ff304f1c005d4cf6248070ca4d3610f201abb323
朱晨艺 <zcy@unisolution.cn>
2024-08-23 08:46:55 +0800

    start

--- module_frame/src/main/java/com/example/module_frame/extend/CameraHelper.kt
+++ module_frame/src/main/java/com/example/module_frame/extend/CameraHelper.kt
@@ -1,10 +1,11 @@
 package com.example.module_frame.extend
 
-
+import com.dylanc.longan.startActivity
+import com.example.camera.PreviewViewActivity
 
 
 object CameraHelper {
     fun  startPreviewActivity(){
-
+           startActivity<PreviewViewActivity>()
     }
 }
